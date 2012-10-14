138a046f7308ef5b7a46ade1a7ffb69ecc3fd9e3
fedik <getthesite@gmail.com>
2012-10-14 12:25:51 +0300

    update template for Joomla! 3.0

--- fullajax_tmpl/index.php
+++ fullajax_tmpl/index.php
@@ -1,22 +1,24 @@
 <?php
 /**
- * @version		index.php  2011-08-06
+ * @version		index.php  2012.10.03
  * @package		Add FullAjax Plugin
  * @author  Fedik
  * @email	getthesite@gmail.com
  * @link    http://www.getsite.org.ua
  * @license	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
  */
 defined('_JEXEC') or die;
-$app = JFactory::getApplication();
+
+$contid = JFactory::getApplication()->input->get('plg_fullajax_contid', 'forajax');
 ?>
-<html><head><jdoc:include type="head" /></head>
+<html>
+<head>
+	<jdoc:include type="head" />
+</head>
 <body>
-<!-- :ax:<?php echo $app->get('fullAjaxContId','forajax'); ?>:begin: //-->
-<?php	if ($this->getBuffer('message')) : ?>
-<div class="error"><jdoc:include type="message" /></div>
-<?php endif; ?>
-<jdoc:include type="component" />
-<!-- :ax:<?php echo $app->get('fullAjaxContId','forajax'); ?>:end: //-->
-</body></html>
+	<!-- :ax:<?php echo $contid; ?>:begin: //-->
+	<jdoc:include type="message" />
+	<jdoc:include type="component" />
+	<!-- :ax:<?php echo $contid; ?>:end: //-->
+</body>
+</html>
